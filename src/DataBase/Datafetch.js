a5b466c952258ababe1d416303e9629dde209e69
yash goel <yashgoel236437@gmail.com>
2022-03-18 14:57:24 +0530

    added folder for api , database, and environment

---
+++ src/DataBase/Datafetch.js
@@ -0,0 +1,29 @@
+/**
+ * Abhishek this Class is your responsibility
+ * sending index of the image (in order of the csv file ) in constructor 
+ * then fetching from mongo db server then making object of the info
+ * I am expecting the Data in object 
+ * you can check out the getData() function for the way the 
+ * information is returned
+ */
+
+
+export default class Database{
+    constructor(index){
+        this.index = index
+        console.log("initiated")
+    }
+
+    getData(){
+        return {
+            // csv file contains scientific name Please change it to common name 
+            "name" : "indian cobra", 
+            "location" : "India",
+            "continent" : "Asia",
+            "Venomous" : "yes",
+            "length" : "2-3 m",
+            "Description": "It is distinct from the king cobra which belong ..." 
+        }
+    }
+
+}
